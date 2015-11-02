e2799a93ae2562b291959c3b7b9b03c8f6d43fc9
Leo Yuriev <leo@yuriev.ru>
2015-11-02 12:55:52 +0300

    HL++: refine read-related probes.
    Change-Id: I5e2d2e4d376e57dbad704a2123641b283fd807fb

--- HL2015.mk
+++ HL2015.mk
@@ -35,7 +35,7 @@ $(1).pass-1.log: $(EXE)
 	&& rm -rf $(DATA) $(CSV)/pass1-$(1)*.csv run.txt \
 	&& mkdir -p $(CSV) $(DATA) \
 	&& $(EXE) -D $(1) -p $(DATA) -m nosync -n $(N)0 -B set -C $(CSV)/pass1- $(LOGOUT) run.txt \
-	&& $(EXE) -D $(1) -p $(DATA) -c -m lazy -n $(N)0 -B get,iterate,crud -w 1 -r 4 -C $(CSV)/pass1- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -c -m lazy -n $(N)0 -B get,iterate,crud -w 1 -C $(CSV)/pass1- $(LOGOUT) run.txt \
 	&& mv run.txt $$@ || (echo "failed $$@" > /dev/stderr && mv run.txt $$@.failed && exit 1)
 
 # Второй прогон:
@@ -50,11 +50,11 @@ $(1).pass-2.log: $(EXE)
 	&& rm -rf $(DATA) $(CSV)/pass2-$(1)*.csv $(CSV)/passR*-$(1)*.csv run.txt \
 	&& mkdir -p $(CSV) $(DATA) \
 	&& $(EXE) -D $(1) -p $(DATA) -m nosync -n $(N)00 -B set $(LOGOUT) run.txt \
-	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N)00 -B get,iterate,crud -w 1 -r 4 -C $(CSV)/pass2- $(LOGOUT) run.txt \
-	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N)00 -B get,iterate -r 1 -C $(CSV)/passR1- $(LOGOUT) run.txt \
-	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N)00 -B get,iterate -r 2 -C $(CSV)/passR2- $(LOGOUT) run.txt \
-	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N)00 -B get,iterate -r 4 -C $(CSV)/passR4- $(LOGOUT) run.txt \
-	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N)00 -B get,iterate -r 8 -C $(CSV)/passR8- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N)00 -B get,iterate,crud -w 1 -C $(CSV)/pass2- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -m nosync -n $(N)00 -B get -C $(CSV)/passR1- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -m nosync -n $(N)00 -B get -r 2 -C $(CSV)/passR2- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -m nosync -n $(N)00 -B get -r 4 -C $(CSV)/passR4- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -m nosync -n $(N)00 -B get -r 8 -C $(CSV)/passR8- $(LOGOUT) run.txt \
 	&& mv run.txt $$@ || (echo "failed $$@" > /dev/stderr && mv run.txt $$@.failed && exit 2)
 
 # Третий прогон:
@@ -67,7 +67,7 @@ $(1).pass-3.log: $(EXE)
 	&& rm -rf $(DATA) $(CSV)/pass3-$(1)*.csv run.txt \
 	&& mkdir -p $(CSV) $(DATA) \
 	&& $(EXE) -D $(1) -p $(DATA) -m nosync -n $(N) -B set $(LOGOUT) run.txt \
-	&& $(EXE) -D $(1) -p $(DATA) -c -m sync -n $(N) -B get,iterate,crud -w 1 -r 4 -C $(CSV)/pass3- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -c -m sync -n $(N) -B get,iterate,crud -w 1 -C $(CSV)/pass3- $(LOGOUT) run.txt \
 	&& mv run.txt $$@ || (echo "failed $$@" > /dev/stderr && mv run.txt $$@.failed && exit 3)
 
 endef
