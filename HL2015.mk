f19c68444478b24ed27e3a257992a724eecf8a12
Leo Yuriev <leo@yuriev.ru>
2015-11-02 04:49:24 +0300

    HL++: adds read-scale probe.
    Change-Id: Ibfd37346861cfb9e050962ba7dd72e7fa0a3d6b5

--- HL2015.mk
+++ HL2015.mk
@@ -20,14 +20,14 @@ $(EXE):
 
 define rule
 
-## Первый прогон:
-## - делаем вид что импортируем данные (поэтому nosync)
-## - далее подаем нагрузку и замеряем
-##
-## Эти данные попадают:
-##  - в слайд №3 "Рекорды"
-##  - в cлайд №31 "LAZY + CRUD * 10**5"
-##  - в слайд №34 "Стоимость" (замер ресурсов через time)
+# Первый прогон:
+# - делаем вид что импортируем данные (поэтому nosync)
+# - далее подаем нагрузку и замеряем
+#
+# Эти данные попадают:
+#  - в слайд №3 "Рекорды"
+#  - в cлайд №31 "LAZY + CRUD * 10**5"
+#  - в слайд №34 "Стоимость" (замер ресурсов через time)
 $(1): $(1).pass-1.log $(1).pass-2.log $(1).pass-3.log
 
 $(1).pass-1.log: $(EXE)
@@ -38,27 +38,30 @@ $(1).pass-1.log: $(EXE)
 	&& $(EXE) -D $(1) -p $(DATA) -c -m lazy -n $(N)0 -B get,iterate,crud -w 1 -r 4 -C $(CSV)/pass1- $(LOGOUT) run.txt \
 	&& mv run.txt $$@ || (echo "failed $$@" > /dev/stderr && mv run.txt $$@.failed && exit 1)
 
-## Второй прогон:
-## - просто CRUD на 10**6 с не-синхронной записью
-## - потом на этой базе показываем масштабирование по CPU
-##
-## Эти данные попадают:
-## - в cлайд №32 "NOSYNC + CRUD * 10**6"
-## - в слайд №29 "Разгоняется"
+# Второй прогон:
+# - просто CRUD на 10**6 с не-синхронной записью
+# - потом на этой базе показываем масштабирование по CPU
+#
+# Эти данные попадают:
+# - в cлайд №32 "NOSYNC + CRUD * 10**6"
+# - в слайд №29 "Разгоняется"
 $(1).pass-2.log: $(EXE)
 	@ echo "$$@..." \
-	&& rm -rf $(DATA) $(CSV)/pass2-$(1)*.csv run.txt \
+	&& rm -rf $(DATA) $(CSV)/pass2-$(1)*.csv $(CSV)/passR*-$(1)*.csv run.txt \
 	&& mkdir -p $(CSV) $(DATA) \
 	&& $(EXE) -D $(1) -p $(DATA) -m nosync -n $(N)00 -B set $(LOGOUT) run.txt \
 	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N)00 -B get,iterate,crud -w 1 -r 4 -C $(CSV)/pass2- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N)00 -B get,iterate -r 1 -C $(CSV)/passR1- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N)00 -B get,iterate -r 2 -C $(CSV)/passR2- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N)00 -B get,iterate -r 4 -C $(CSV)/passR4- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N)00 -B get,iterate -r 8 -C $(CSV)/passR8- $(LOGOUT) run.txt \
 	&& mv run.txt $$@ || (echo "failed $$@" > /dev/stderr && mv run.txt $$@.failed && exit 2)
 
-
-## Третий прогон:
-## - просто CRUD на 10000 с синхронной записью
-##
-## Эти данные попадают:
-##  - в cлайд №30 "SYNC + CRUD * 10**4"
+# Третий прогон:
+# - просто CRUD на 10000 с синхронной записью
+#
+# Эти данные попадают:
+#  - в cлайд №30 "SYNC + CRUD * 10**4"
 $(1).pass-3.log: $(EXE)
 	@ echo "$$@..." \
 	&& rm -rf $(DATA) $(CSV)/pass3-$(1)*.csv run.txt \
