98f3d4905dc619dd186b37ba5f20d2d4275ed90c
Leo Yuriev <leo@yuriev.ru>
2015-11-06 16:08:12 +0300

    HL++: post-update for actual slides content.
    Change-Id: I3f88ba249c88c7d64e60f7b62eab51a4e2182975

--- HL2015.mk
+++ HL2015.mk
@@ -1,4 +1,4 @@
-N1	?= 1000000
+N1	?= 500000
 N2	?= 1000000
 N3	?= 10000
 
@@ -23,8 +23,7 @@ $(EXE):
 define rule
 
 # Первый прогон:
-# - первый CRUD для заполнения и замера "стоимости" (ресурсов)
-# - далее подаем нагрузку и замеряем "качество" (задержку)
+# - CRUD замера "стоимости" (ресурсов) и "качества" (задержка)
 #
 # Эти данные попадают:
 #  - в слайд №3 "Рекорды"
@@ -34,15 +33,13 @@ $(1): $(1).pass-1.log $(1).pass-2.log $(1).pass-3.log
 
 $(1).pass-1.log: $(EXE)
 	@ echo "$$@..." \
-	&& rm -rf $(DATA) $(CSV)/1*-$(1)*.csv run.txt \
+	&& rm -rf $(DATA) $(CSV)/1-$(1)*.csv run.txt \
 	&& mkdir -p $(CSV) $(DATA) \
-	&& $(EXE) -D $(1) -p $(DATA) -m lazy -n $(N1) -B crud -C $(CSV)/1a- $(LOGOUT) run.txt \
-	&& $(EXE) -D $(1) -p $(DATA) -c -m lazy -n $(N1) -B get,iterate,crud -w 1 -C $(CSV)/1b- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -m lazy -n $(N1) -B crud -C $(CSV)/1- $(LOGOUT) run.txt \
 	&& mv run.txt $$@ || (echo "failed $$@" > /dev/stderr && mv run.txt $$@.failed && exit 1)
 
 # Второй прогон:
-# - сначала заполнение
-# - далее CRUD на 10**6 с не-синхронной записью
+# - CRUD на 10**6 с не-синхронной записью
 # - потом на этой базе показываем масштабирование по CPU
 #
 # Эти данные попадают:
@@ -52,8 +49,7 @@ $(1).pass-2.log: $(EXE)
 	@ echo "$$@..." \
 	&& rm -rf $(DATA) $(CSV)/2*-$(1)*.csv run.txt \
 	&& mkdir -p $(CSV) $(DATA) \
-	&& $(EXE) -D $(1) -p $(DATA) -m nosync -n $(N2) -B set $(LOGOUT) run.txt \
-	&& $(EXE) -D $(1) -p $(DATA) -c -m nosync -n $(N2) -B get,iterate,crud -w 1 -C $(CSV)/2a- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -m nosync -n $(N2) -B crud -C $(CSV)/2a- $(LOGOUT) run.txt \
 	&& $(EXE) -D $(1) -p $(DATA) -n $(N2) -B get -C $(CSV)/2b1- $(LOGOUT) run.txt \
 	&& $(EXE) -D $(1) -p $(DATA) -n $(N2) -B get -r 2 -C $(CSV)/2b2- $(LOGOUT) run.txt \
 	&& $(EXE) -D $(1) -p $(DATA) -n $(N2) -B get -r 4 -C $(CSV)/2b4- $(LOGOUT) run.txt \
@@ -69,8 +65,7 @@ $(1).pass-3.log: $(EXE)
 	@ echo "$$@..." \
 	&& rm -rf $(DATA) $(CSV)/3-$(1)*.csv run.txt \
 	&& mkdir -p $(CSV) $(DATA) \
-	&& $(EXE) -D $(1) -p $(DATA) -m nosync -n $(N3) -B set $(LOGOUT) run.txt \
-	&& $(EXE) -D $(1) -p $(DATA) -c -m sync -n $(N3) -B get,iterate,crud -w 1 -C $(CSV)/3- $(LOGOUT) run.txt \
+	&& $(EXE) -D $(1) -p $(DATA) -m sync -n $(N3) -B crud -C $(CSV)/3- $(LOGOUT) run.txt \
 	&& mv run.txt $$@ || (echo "failed $$@" > /dev/stderr && mv run.txt $$@.failed && exit 3)
 
 endef
