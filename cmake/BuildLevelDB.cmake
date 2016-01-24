55f2d74f2c2d7402cc0f0a3e84f31f1527e54678
Leo Yuriev <leo@yuriev.ru>
2016-01-25 00:04:53 +0300

    ioarena: avoid cmake folly with libleveldb so-suffixes.
    Change-Id: I46e8b88f4b5c4e1efd15281a3b2ed724376db70e

--- cmake/BuildLevelDB.cmake
+++ cmake/BuildLevelDB.cmake
@@ -5,24 +5,24 @@ macro(build_leveldb)
 	separate_arguments(LEVELDB_OPTS)
 	if (${PROJECT_BINARY_DIR} STREQUAL ${PROJECT_SOURCE_DIR})
 		add_custom_command(
-			OUTPUT ${PROJECT_BINARY_DIR}/db/leveldb/libleveldb.so.1
-			COMMAND ${CMAKE_MAKE_PROGRAM} -j ${MAKE_NJOB} ${LEVELDB_OPTS} -C ${PROJECT_BINARY_DIR}/db/leveldb libleveldb.so.1 libleveldb.so
+			OUTPUT ${PROJECT_BINARY_DIR}/db/leveldb/libleveldb.so
+			COMMAND ${CMAKE_MAKE_PROGRAM} -j ${MAKE_NJOB} ${LEVELDB_OPTS} -C ${PROJECT_BINARY_DIR}/db/leveldb
 			DEPENDS ${CMAKE_BINARY_DIR}/CMakeCache.txt
 		)
 	else()
 		add_custom_command(
-			OUTPUT ${PROJECT_BINARY_DIR}/db/leveldb/libleveldb.so.1
+			OUTPUT ${PROJECT_BINARY_DIR}/db/leveldb/libleveldb.so
 			COMMAND ${CMAKE_COMMAND} -E make_directory ${PROJECT_BINARY_DIR}/db/leveldb
 			COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/db/leveldb ${PROJECT_BINARY_DIR}/db/leveldb
-			COMMAND ${CMAKE_MAKE_PROGRAM} -j ${MAKE_NJOB} ${LEVELDB_OPTS} -C ${PROJECT_BINARY_DIR}/db/leveldb libleveldb.so.1 libleveldb.so
+			COMMAND ${CMAKE_MAKE_PROGRAM} -j ${MAKE_NJOB} ${LEVELDB_OPTS} -C ${PROJECT_BINARY_DIR}/db/leveldb
 			DEPENDS ${PROJECT_BINARY_DIR}/CMakeCache.txt
 		)
 	endif()
 	add_custom_target(libleveldb ALL
-		DEPENDS ${PROJECT_BINARY_DIR}/db/leveldb/libleveldb.so.1
+		DEPENDS ${PROJECT_BINARY_DIR}/db/leveldb/libleveldb.so
 	)
 	message(STATUS "Use shipped LevelDB: ${PROJECT_SOURCE_DIR}/db/leveldb")
-	set (LEVELDB_LIBRARIES "${PROJECT_BINARY_DIR}/db/leveldb/libleveldb.so.1")
+	set (LEVELDB_LIBRARIES "${PROJECT_BINARY_DIR}/db/leveldb/libleveldb.so")
 	set (LEVELDB_FOUND 1)
 	add_dependencies(build_libs libleveldb)
 endmacro(build_leveldb)
