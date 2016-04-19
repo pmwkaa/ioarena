245433edb326dffad9d9f95a29c73e381bdf3ab6
Leo Yuriev <leo@yuriev.ru>
2016-04-19 16:37:42 +0300

    ioarena: cleanup LMDB build options.
    Change-Id: I27ba588932331303518b2e5819a9c2d4520860f7

--- cmake/BuildLMDB.cmake
+++ cmake/BuildLMDB.cmake
@@ -1,12 +1,10 @@
 
 macro(build_lmdb)
 	set(LMDB_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/db/lmdb/libraries/liblmdb)
-	set(LMDB_OPTS XCFLAGS="-DNDEBUG")
-	separate_arguments(LMDB_OPTS)
 	if (${PROJECT_BINARY_DIR} STREQUAL ${PROJECT_SOURCE_DIR})
 		add_custom_command(
 			OUTPUT ${PROJECT_BINARY_DIR}/db/lmdb/libraries/liblmdb/liblmdb.so
-			COMMAND ${CMAKE_MAKE_PROGRAM} -j ${MAKE_NJOB} ${LMDB_OPTS} -C ${PROJECT_BINARY_DIR}/db/lmdb/libraries/liblmdb liblmdb.so
+			COMMAND ${CMAKE_MAKE_PROGRAM} -j ${MAKE_NJOB} -C ${PROJECT_BINARY_DIR}/db/lmdb/libraries/liblmdb liblmdb.so
 			DEPENDS ${CMAKE_BINARY_DIR}/CMakeCache.txt
 		)
 	else()
@@ -14,7 +12,7 @@ macro(build_lmdb)
 			OUTPUT ${PROJECT_BINARY_DIR}/db/lmdb/libraries/liblmdb/liblmdb.so
 			COMMAND ${CMAKE_COMMAND} -E make_directory ${PROJECT_BINARY_DIR}/db/lmdb/libraries/liblmdb
 			COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/db/lmdb/libraries/liblmdb ${PROJECT_BINARY_DIR}/db/lmdb/libraries/liblmdb
-			COMMAND ${CMAKE_MAKE_PROGRAM} -j ${MAKE_NJOB} ${LMDB_OPTS} -C ${PROJECT_BINARY_DIR}/db/lmdb/libraries/liblmdb liblmdb.so
+			COMMAND ${CMAKE_MAKE_PROGRAM} -j ${MAKE_NJOB} -C ${PROJECT_BINARY_DIR}/db/lmdb/libraries/liblmdb liblmdb.so
 			DEPENDS ${PROJECT_BINARY_DIR}/CMakeCache.txt
 		)
 	endif()
